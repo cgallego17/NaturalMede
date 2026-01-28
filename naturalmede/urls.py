013bb844c02b36113194dd78aafc3634d9bdf519
cgallego17 <crisnal08@gmail.com>
2026-01-28 15:03:50 -0500

    Add password visibility toggle and password reset links to login forms

--- naturalmede/urls.py
+++ naturalmede/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
@@ -34,6 +35,12 @@ urlpatterns = [
     path('api/orders/', include('orders.api_urls')),
     path('api/customers/', include('customers.api_urls')),
     path('api/pos/', include('pos.api_urls')),
+    
+    # Password reset URLs
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='registration/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='registration/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='registration/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='registration/password_reset_complete.html'), name='password_reset_complete'),
 ]
 
 if settings.DEBUG:
