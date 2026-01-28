f39ae8cbf6d7bf6ab3fa3e5cd7b567645072bd2a
cgallego17 <crisnal08@gmail.com>
2026-01-28 15:28:24 -0500

    Adjust product card image dimensions and add production hosts to allowed hosts configuration

--- naturalmede/settings.py
+++ naturalmede/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-naturalmede-developme
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True').lower() == 'true'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'testserver']
+ALLOWED_HOSTS = ['64.227.63.64', 'naturalmed.com.co', 'www.naturalmed.com.co', 'localhost', '127.0.0.1']
 
 
 # Application definition
